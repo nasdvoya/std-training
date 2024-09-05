3d098d3f4a5d3ff73b4e0e688d13a16f870c3a57
sasha <nasdvoya@proton.me>
2024-09-05 07:16:55 +0100

    Dev env

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+    fhs = pkgs.buildFHSUserEnv {
+      name = "fhs-shell";
+      targetPkgs = pkgs: with pkgs; [
+        gcc
+
+        pkg-config
+        libclang.lib
+        gnumake
+        cmake
+        ninja
+
+        git
+        wget
+
+        rustup
+        cargo-generate
+
+        espflash
+        python3
+        python3Packages.pip
+        python3Packages.virtualenv
+        ldproxy
+      ];
+    };
+  in {
+    devShells.${pkgs.system}.default = fhs.env;
+  };
+}
